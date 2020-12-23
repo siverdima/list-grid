24372e7f792eca3a79d5f603167b31638a7f5796
dima <dima@eastoner.net>
2020-12-23 15:34:50 +0200

    Remove timeSince

--- src/app/products-list-grid/products-list-grid.component.ts
+++ src/app/products-list-grid/products-list-grid.component.ts
@@ -19,34 +19,4 @@ export class ProductsListGridComponent implements OnInit {
   public onTitleClicked(product: Product): void {
     this.productClicked.emit(product);
   }
-
-  public timeSince(date: Date): string {
-    const now = new Date();
-    const seconds = Math.floor(
-      (now.getMilliseconds() - new Date(date).getMilliseconds()) / 1000
-    );
-
-    let interval = seconds / 31536000;
-
-    if (interval > 1) {
-      return Math.floor(interval) + ' years';
-    }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-      return Math.floor(interval) + ' months';
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-      return Math.floor(interval) + ' days';
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-      return Math.floor(interval) + ' hours';
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-      return Math.floor(interval) + ' minutes';
-    }
-    return Math.floor(seconds) + ' seconds';
-  }
 }
